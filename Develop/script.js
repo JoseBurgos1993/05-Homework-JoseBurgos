47a08c561c1843c2af3426f78c6c30326561563d
JoseBurgos1993 <burgosjd1993@gmail.com>
2020-06-19 18:00:03 -0400

    Finished with javascript file. Everything is commented

--- Develop/script.js
+++ Develop/script.js
@@ -2,18 +2,67 @@
 // By Jose Burgos
 
 $("document").ready(function(){
-
+    // Initializes an array for our saved text
     var savedText = ["","","","","","","","","","",""]; // 24 items
 
+    // Puts today's date near the top of the screen
     $("#currentDay").text(moment().format("MMMM Do YYYY"));
 
+    // Returns the correct time to display on the left side \\
+    function getTime(i){
+        // AM. Adds 8 to 'i' because 'i' starts at 0, but our time starts at 8am
+        if(i < 4) { return (i+8) + ":00 AM"; }
+        // PM
+        else{
+            // By this point, 'i' will be 5, so we substract 4 to continue with 1pm and onwards
+            if(i != 4){ return (i-4) + ":00 PM"; }
+            // Noon
+            else{ return "12:00 PM"; }
+        }
+    }
+
+    // Returns class that will paint the text area the correct color based on time
+    function colorBlock(i){
+        const hour = moment().get("hour");
+        if(i < hour){
+            return "past";
+        } else if(i == hour){
+            return "present";
+        } else{
+            return "future";
+        }
+    }
+
+    // Creates Text Area \\
+    function createTextArea(i){
+        const textArea = $("<textarea>");
+        textArea.addClass("form-control " + colorBlock(i+8));
+        textArea.attr("rows", "3");
+        textArea.val(savedText[i]);
+        return textArea;
+    }
+
+    // Creates Save Button \\
+    function createButton(){
+        // Creates new button and applies the appropriate classes
+        const newButton = $("<button>");
+        newButton.addClass("btn btn-primary saveBtn");
+
+        // This creates the lock icon that appears on the button
+        const lock = $("<i>");
+        lock.addClass("fa fa-lock");
+        newButton.append(lock);
+
+        return newButton;
+    }
 
 
-
-
-    // Read and write from local storage \\
+    // Read from local storage \\
     function readData(){
         for(let i = 0; i < 11; i++){
+            // Items are saved as 'savedText8','savedText9', etc.
+            // Hence why we add 8 to 'i' when reading from memory. The reason I don't just start my
+            // loop at 8 is because we would then have to subtract 8 when accessing the savedText array.
             let temp = localStorage.getItem("savedText" + (i+8));
             if(temp !== null){
                 savedText[i] = temp;
@@ -21,26 +70,41 @@ $("document").ready(function(){
         }
     }
 
-    // Colors and writes to blocks
-    function setupBlocks(){
-        const hour = moment().get('hour');
-        let index = 8;
-        $(".row").each(function(){
-            let block = $(this).find("textarea");
+    // Create Blocks \\
+    function createBlocks(){
+        // Finds our container
+        const container = $(".container");
 
-            if(index < hour){
-                block.addClass("past");
-            } else if(index == hour){
-                block.addClass("present");
-            } else{
-                block.addClass("future");
-            }
+        for(let i = 0; i < 11; i++){
+            // Create a new row
+            const newRow = $("<div>");
+            newRow.addClass("row");
 
-            block.val(savedText[index-8]); // Writes save data into the text area
-            index++;
-        });
+            // Create the 3 divs for the row
+            const div1 = $("<div>");
+            const div2 = $("<div>");
+            const div3 = $("<div>");
+
+            // Apply the appropriate classes to each div
+            div1.addClass("col-md-2 time-block hour");
+            div2.addClass("col-md-8");
+            div3.addClass("col-md-2");
+
+            // Calls functions that will add content to each div
+            div1.text(getTime(i));
+            div2.append(createTextArea(i));
+            div3.append(createButton());
+
+            // Appends the divs onto the row, then appends the row onto the container
+            newRow.append(div1);
+            newRow.append(div2);
+            newRow.append(div3);
+            container.append(newRow);
+        }
     }
-
+    // Functions to call when starting the page \\
+    readData();
+    createBlocks();
 
     // Save Button Event Listener \\
     $(".saveBtn").on("click", function(){
@@ -51,121 +115,4 @@ $("document").ready(function(){
             index++;
         });
     });
-
-    readData();
-    setupBlocks();
-
-    /*
-    function createBlocks(){
-        // Creates all 24 blocks
-        const container = $(".container");
-        for(let i = 0; i < 24; i++){
-            const newRow = $("<div>");
-            newRow.addClass("row");
-            createColumns(newRow, i);
-            container.append(newRow);
-        }
-        container.append($("<br>"));
-    }
-
-    function createColumns(newRow, index){
-        // Creates the 3 columns for the blocks
-
-        const col1 = $("<div>");
-        col1.addClass("col-md-2");
-        col1.text(convertTime(index));
-        col1.css("text-align", "right");
-        const col2 = $("<div>");
-        col2.addClass("col-md-8");
-        const input = $("<input>");
-        input.css("width", "100%");
-        input.css("height", "100%");
-        input.css("background-color", colorBlocks(index));
-        input.css("outline-color", "black");
-        input.attr("id", "form"+index);
-        input.rows = "3";
-        input.val(savedText[index]);
-        console.log(input.value);
-        col2.css("background-color", colorBlocks(index));
-        col2.css("padding", "2px");
-        col2.append(input);
-
-        const col3 = $("<div>");
-        col3.addClass("col-md-2");
-        const newButton = $("<button>");
-        //newButton.type = "button";
-        newButton.addClass("btn btn-primary");
-        newButton.html("<i class='fa fa-lock'></i>");
-        newButton.css("width",'100%');
-        newButton.css("height",'100%');
-        newButton.attr("data-letter", index);
-        //newButton.text(newButton.attr("data-letter"));
-        col3.append(newButton);
-
-        newRow.append(col1);
-        newRow.append(col2);
-        newRow.append(col3);
-
-    }
-
-    // Reads data from local storage to write into blocks \\
-    function readData(){
-        console.log(savedText);
-        for(let i = 0; i < 24; i++){
-            let temp = localStorage.getItem("savedText" + i);
-            if(temp === null){
-                savedText[i] = " ";
-            } else{
-                savedText[i] = temp;
-            }
-        }
-        console.log(savedText);
-    }
-
-    // Colors the blocks \\
-    function colorBlocks(index){
-        //const hour = parseInt(moment().get('hour'));
-        const hour = moment().get('hour');
-        console.log(hour);
-        if(index < hour){
-            return "lightgray"; // PAST
-        } else if(index == hour){
-            return "lightcoral"; // PRESENT
-        } else{
-            return "greenyellow"; // FUTURE
-        }
-    }
-
-    function convertTime(index){
-        // Convert index value 0-23 into an AM/PM format to output onto the webpage
-        if(index == 0){
-            return "12:00 AM";
-        } else if(index > 0 && index <12){
-            return index + ":00 AM";
-        } else if(index == 12){
-            return "12:00 PM"
-        } else if(index > 12){
-            return (index - 12) + ":00 PM"
-        }
-    }
-    readData();
-    createBlocks();
-    colorBlocks();
-
-    // Save Button \\
-    $("button").on("click", function(){
-        // Activates when save button is pressed. Saves the text in the textfield to local storage. Use array?
-        // Use something like $(this).index to the 0-23 value.
-        console.log("You pressed index " + $(this).attr("data-letter"));
-        //const row = $(".container").children()[parseInt($(this).attr("data-letter"))];
-        //console.log($(".container"));
-
-        const index = $(this).attr("data-letter");
-        const form = $("#form"+index);
-        const text = form.val();//"Hello good sir";
-        savedText[index] = text;
-        localStorage.setItem("savedText" + index, savedText[index]);
-
-    });
-    */
 });
